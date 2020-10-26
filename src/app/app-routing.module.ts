ba964eae1c498c4e298848e14c7fd215cd3fb7af
Ashish Kumar <ashish.kumar@moonraft.com>
2020-10-27 03:17:20 +0530

    Onbording route added to app route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OnboardingComponent } from './features/onboarding/components/onboarding.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'onboarding',
+    pathMatch: 'full',
+    //lazy loading not added as this is the default url anyhow
+    component: OnboardingComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/onboarding',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/onboarding',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
