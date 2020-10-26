ff03ea1b08924061cfb8805a6caee3fcd2fc97ea
Ashish Kumar <ashish.kumar@moonraft.com>
2020-10-27 04:30:44 +0530

    Adding negative cases for onboaring initial setup and default tabs selection

--- src/app/features/onboarding/components/onboarding.component.spec.ts
+++ src/app/features/onboarding/components/onboarding.component.spec.ts
@@ -5,12 +5,39 @@ import { OnboardingComponent } from './onboarding.component';
 describe('OnboardingComponent', () => {
   let component: OnboardingComponent;
   let fixture: ComponentFixture<OnboardingComponent>;
+  let onboardingType = [
+    {
+      key: 'signup',
+      value: 'Sign Up',
+    },
+    {
+      key: 'login',
+      value: 'Log In',
+    },
+  ];
+  let onboardingInputReference = {
+    name: {
+      label: 'Name',
+      type: 'text',
+    },
+    employeeId: {
+      label: 'Employee ID',
+      type: 'text',
+    },
+    emailId: {
+      label: 'Email ID',
+      type: 'text',
+    },
+    password: {
+      label: 'Password',
+      type: 'password',
+    },
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OnboardingComponent ]
-    })
-    .compileComponents();
+      declarations: [OnboardingComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +46,20 @@ describe('OnboardingComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Onboarding: should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('Onboarding: should have an private Onboarding type array', () => {
+    // @ts-ignore
+    expect(component.ONBOARDING_TYPE).toEqual(onboardingType);
+  });
+  it('Onboarding: should have a list of inputs', () => {
+    // @ts-ignore
+    expect(component.ONBOARDING_INPUT_REF).toEqual(onboardingInputReference);
+  });
+  it('Onboarding: should select the signup tab by default', () => {
+    component.ngOnInit();
+    // @ts-ignore
+    expect(component.selectedTab).toEqual(component.ONBOARDING_TYPE[0]);
   });
 });
