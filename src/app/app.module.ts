699cd876fcff092a7a22dd89c61797f0f33fe82d
Ashish Kumar <ashish.kumar@moonraft.com>
2020-10-29 13:20:17 +0530

    Http intercept added to add tokens with each service call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { JwtInterceptor } from './shared/helpers/interceptors/jwt.interceptor';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +17,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CoreModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/shared/helpers/interceptors/jwt.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../../environments/environment';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // add auth header with jwt if user is logged in and request is to the api url
+    const user = this.authService.currentUserData();
+    const isLoggedIn = user && user.token;
+    const isApiUrl = request.url.startsWith(environment.restApiUrl);
+    if (isLoggedIn && isApiUrl) {
+      request = request.clone({
+        setHeaders: { Authorization: `Hacknews-fake-token-${user.token}` },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
